70caaad283f115d829fab243ce09bfda7959821f
Jerex Salingujay <jerex@peregrineconsultinggroup.com>
2021-08-02 20:50:53 +0800

    Fix WebView Stiaa Site

--- src/components/WebView.js
+++ src/components/WebView.js
@@ -5,14 +5,16 @@ class MyWeb extends Component {
   render() {
     return (
       <WebView
-        source={{ uri: 'https://stiaa.miwcity.net' }}
-        style={{ marginTop: 20 }}
+        originWhitelist={['*']}
         javaScriptEnabled={true}
         domStorageEnabled={true}
         sharedCookiesEnabled={true}
+        source={{ uri: 'https://stiaa.miwcity.net' }}
+        style={{height:"100%", width:"100%" }}
       />
     );
   }
 }
 
 export default MyWeb;
+
